c5125da49705ec5b93629c94810b6c040032ca74
Gui Iribarren <gui@altermundi.net>
2021-04-01 02:56:10 +0200

    install victoria-metrics

--- install.sh
+++ install.sh
@@ -15,3 +15,20 @@ systemctl daemon-reload
 systemctl enable grafana-server
 systemctl start grafana-server
 # end Grafana
+
+# Install VictoriaMetrics (unavailable in buster as of 2021/03/31)
+cd /tmp
+
+wget http://ftp.debian.org/debian/pool/main/libz/libzstd/libzstd1_1.4.8+dfsg-2.1_arm64.deb
+dpkg --skip-same-version -i libzstd1_1.4.8+dfsg-2.1_arm64.deb
+rm libzstd1_1.4.8+dfsg-2.1_arm64.deb
+
+wget http://ftp.debian.org/debian/pool/main/v/victoriametrics/victoria-metrics_1.53.1+ds-1+b1_arm64.deb
+dpkg --skip-same-version -i victoria-metrics_1.53.1+ds-1+b1_arm64.deb
+rm victoria-metrics_1.53.1+ds-1+b1_arm64.deb
+
+( cd files/ ; cp --archive --backup=numbered ./etc/default/victoria-metrics /etc/default/victoria-metrics )
+
+systemctl restart victoria-metrics
+
+# end VictoriaMetrics
